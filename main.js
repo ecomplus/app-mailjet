767350cf53d84d94fed4e0a9ed5ba1eac1d98ad1
Talisson Ferreira <talissonf@gmail.com>
2019-01-11 15:42:52 -0200

    Update main

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4200
+const port = process.env.PORT || 5000
 
 require('./bin/uncaughtException')
 
