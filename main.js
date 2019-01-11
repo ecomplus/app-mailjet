6a35c7976179b86c67d8f6bf4dd4846e9a47e6fe
Talisson Ferreira <talissonf@gmail.com>
2019-01-11 12:59:32 -0200

    import jobs

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 'use strict'
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4200
 
 require('./bin/uncaughtException')
 
@@ -10,3 +11,5 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(require('./lib/routes'))
 app.listen(port)
+
+require('./lib/mailjet/jobs')
